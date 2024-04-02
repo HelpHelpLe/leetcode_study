83779d27d7928e5f8a2d330426456b3e44057ff6
HelpHelpLe <you@example.com>
2024-04-02 05:39:12 +0000

    findTargetSumWays motify the initial way

--- findTargetSumWays/main.cpp
+++ findTargetSumWays/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     Solution solution;
-    vector<int> nums = {0,0,0,0,0,0,0,0,1};
+    vector<int> nums = {0, 0, 0, 0, 0, 0, 0, 0, 1};
     int target = 1;
     int ans = solution.findTargetSumWays(nums, target);
     cout << ans << endl;
--- findTargetSumWays/solution.cpp
+++ findTargetSumWays/solution.cpp
@@ -7,19 +7,27 @@ int Solution::findTargetSumWays(vector<int>& nums, int target) {
     for (auto num : nums) {
         sum += num;
     }
+    if (target < 0) {
+        target = 0 - target;
+    }
+    if (target > sum ) {
+        return 0;
+    }
     int size = nums.size();
     vector<int> dp(sum + 1, 0);
     dp[0] = 1;
-    if (nums[0] <= sum) {
-        dp[nums[0]] = 1;
+    int first = nums[0];
+    if (first <= sum) {
+        if (first == 0) {
+            dp[first] = 2;
+        } else {
+            dp[first] = 1;
+        }
     }
     for (int i = 1; i < size; i++) {
         for (int j = sum; j >= nums[i]; j--) {
             dp[j] += dp[j - nums[i]];
         }
-    }
-    if (target < 0) {
-        target = 0 - target;
     }
     int m = sum - target;
     int x = m / 2;
